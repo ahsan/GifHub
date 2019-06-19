68e3987a139cbc507352522df0e32f5137aaab6c
Abdullah Ahsan <ahsan.abdulah@gmail.com>
2019-06-19 19:34:40 +0200

    API url population in nginx conf

--- frontend/src/components/GHFavorites.js
+++ frontend/src/components/GHFavorites.js
@@ -13,7 +13,7 @@ class GHFavorites extends React.Component {
   }
 
   componentDidMount() {
-    const apiUrl = `${process.env.REACT_APP_API_URL}/user?userEmail=${this.props.userEmail}`;
+    const apiUrl = `/api/user?userEmail=${this.props.userEmail}`;
     axios
       .get(apiUrl)
       .then((response) => {
